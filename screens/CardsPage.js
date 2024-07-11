579e5596d029c04497298ee78e4cfbdd2466b70a
bryanjhc <bryanchew22@gmail.com>
2024-07-11 11:19:13 +0800

    added navigation from cards pages to details page

--- screens/CardsPage.js
+++ screens/CardsPage.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Image, View, StyleSheet, ScrollView, TouchableOpacity, Text } from 'react-native';
 import { useAuth } from '../screens/AuthContext';
-import { supabase } from  '../lib/supabase';
+import { supabase } from '../lib/supabase';
 import { Rating } from 'react-native-ratings';
-import { getGlobalRefresh, setGlobalRefresh } from '../global/globVariables.js'
+import { getGlobalRefresh, setGlobalRefresh } from '../global/globVariables.js';
+import { useNavigation } from '@react-navigation/native';
 
 function CardsPage() {
     const [reviews, setReviews] = useState([]);
     const [toiletMapping, setToiletMapping] = useState({});
     const [refreshStatus, setRefreshStatus] = useState(false)
 
+    const navigation = useNavigation()
+
     const fetchReviews = async () => {
         const { data, error } = await supabase
-          .from('reviews')
-          .select('*')
+            .from('reviews')
+            .select('*')
         if (error) {
-          console.log("Error fetching reviews: ", error);
+            console.log("Error fetching reviews: ", error);
         } else {
             const sortedData = data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
             setReviews(sortedData);
@@ -24,7 +27,7 @@ function CardsPage() {
 
     const markerMapping = async () => {
         try {
-            const {data, error} = await supabase
+            const { data, error } = await supabase
                 .from('toilets')
                 .select('*');
             if (error) {
@@ -56,34 +59,36 @@ function CardsPage() {
 
     const ReviewCard = ({ review }) => {
         return (
-          <View style={styles.card}>
-            <Image source={require('../assets/testpic.jpg')} style={styles.image} />
-            <View style={styles.content}>
-              <Text style={styles.name}>{toiletMapping[review.toilet_uuid]}</Text>
-              <Text style={styles.comment}>User: {review.description}</Text>
-              <View style={styles.ratingLocation}>
-                <Rating
-                  type="star"
-                  readonly={true}
-                  startingValue={parseFloat(review.rated)}
-                  ratingCount={5}
-                  imageSize={20}
-                  tintColor={'#FFFFFF'}
-                />
-                <Text style={styles.location}>{review.cubicle_no ? "Cubicle Number " + review.cubicle_no : "Location not specified"}</Text>
-              </View>
-              <View style={styles.likesDislikes}>
-                <TouchableOpacity style={styles.likesButton} onPress={() => handleLike(review.review_uuid)}>
-                    <Text style={styles.likes}>{review.likes} Likes</Text>
+            <View style={styles.card}>
+                <TouchableOpacity onPress={() => handleCardPress(review.toilet_uuid)}>
+                    <Image source={require('../assets/testpic.jpg')} style={styles.image} />
+                    <View style={styles.content}>
+                        <Text style={styles.name}>{toiletMapping[review.toilet_uuid]}</Text>
+                        <Text style={styles.comment}>User: {review.description}</Text>
+                        <View style={styles.ratingLocation}>
+                            <Rating
+                                type="star"
+                                readonly={true}
+                                startingValue={parseFloat(review.rated)}
+                                ratingCount={5}
+                                imageSize={20}
+                                tintColor={'#FFFFFF'}
+                            />
+                            <Text style={styles.location}>{review.cubicle_no ? "Cubicle Number " + review.cubicle_no : "Location not specified"}</Text>
+                        </View>
+                        <View style={styles.likesDislikes}>
+                            <TouchableOpacity style={styles.likesButton} onPress={() => handleLike(review.review_uuid)}>
+                                <Text style={styles.likes}>{review.likes} Likes</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.dislikesButton} onPress={() => handleDislike(review.review_uuid)}>
+                                <Text style={styles.dislikes}>{review.dislikes} Dislikes</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.dislikesButton} onPress={() => handleDislike(review.review_uuid)}>
-                    <Text style={styles.dislikes}>{review.dislikes} Dislikes</Text>
-                </TouchableOpacity>
-              </View>
             </View>
-          </View>
         );
-      };
+    };
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -106,14 +111,34 @@ function CardsPage() {
         markerMapping();
     }, [refreshStatus]);
 
+    const handleCardPress = async (uuid) => {
+        let item = {};
+        try {
+            const { data, error } = await supabase
+                .from('toilets')
+                .select('*')
+                .eq('uuid', uuid);
+            if (error) {
+                console.log("Error retrieving data from toilets table: ", error);
+            } else {
+                item = data[0];
+            }
+        } catch (error) {
+            console.log("Database error, failed to run handleCardPress: ", error);
+        }
+        setTimeout(() => {
+            navigation.navigate('Details', { marker: item });
+        }, 300);
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.pageTitle}>Reviews</Text>
             </View>
             <ScrollView style={styles.scrollContainer}>
-                {reviews.slice(0,5).map((review) => (
-                    <ReviewCard key={review.id} review={review}/>
+                {reviews.slice(0, 5).map((review) => (
+                    <ReviewCard key={review.id} review={review} />
                 ))}
             </ScrollView>
         </View>
