cf91fb43a5a69f7e3f45fd9269a32191c3fb4f45
bryanjhc <bryanchew22@gmail.com>
2024-07-18 20:12:43 +0800

    added cleaner resolve page to display errors and allow cleaners to resolve then

--- global/globVariables.js
+++ global/globVariables.js
@@ -1,6 +1,7 @@
 let globalRefresh = false;
 let globalReviewRefresh = false;
 let globalProfileRefresh = false;
+let globalErrorRefresh = false;
 
 export const setGlobalRefresh = (newValue) => {
     globalRefresh = newValue;
@@ -12,6 +13,11 @@ export const setProfileRefresh = (newValue) => {
     globalReviewRefresh = newValue;
 };
 
+export const setErrorRefresh = (newValue) => {
+    globalErrorRefresh = newValue;
+};
+
 export const getGlobalRefresh = () => globalRefresh;
 export const getReviewRefresh = () => globalReviewRefresh;
 export const getProfileRefresh = () => globalProfileRefresh;
+export const getErrorRefresh = () => globalErrorRefresh;
