2ab0eb2cc8be6031961e7ccc360d0a234a0f9cbe
nictjh <nicholastok0101@gmail.com>
2024-09-04 10:52:23 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = async function (env, argv) {
 
 
 
-// Fix attempt for maps 
+// Fix attempt for maps
 // module.exports = async function (env, argv) {
 //   const config = await createExpoWebpackConfigAsync(env, argv);
 
